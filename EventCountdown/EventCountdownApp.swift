3fe0890a644d34e9cb227aeca699c5ea884757a8
Gr33nEight <natanael.jop.app@gmail.com>
2024-11-20 12:28:37 +0100

    first commit

---
+++ EventCountdown/EventCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventCountdownApp.swift
+//  EventCountdown
+//
+//  Created by Natanael Jop on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct EventCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
